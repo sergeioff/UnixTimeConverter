e7e53efead05a19df15316425b75ceb24c2baec7
sergeioff <sergeioff@mail.ru>
2015-02-25 20:06:44 +0200

    Added printf and some fixes

--- UnixTimeConvert.cpp
+++ UnixTimeConvert.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 const int SYEAR = 31536000,
           SDAY = 86400,
@@ -6,7 +7,7 @@ const int SYEAR = 31536000,
           SMINUTE = 60,
           YEAROFFSET = 1970;
 
-int months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int getYearOffset(long timestamp) {
     return timestamp / SYEAR;
@@ -20,16 +21,16 @@ int getDays(long timestamp) {
     return (timestamp - SYEAR * (getYearOffset(timestamp))) / SDAY;
 }
 
-int getVysDays(int year) {
-    int vys = 0;
+int getLeapDays(int year) {
+    int leapDays = 0;
     for (int i = YEAROFFSET; i < year; i++) {
-        if ((i % 4) == 0) vys++;
+        if ((i % 4) == 0) leapDays++;
     }
-    return vys;
+    return leapDays;
 }
 
 int getMonth(long timestamp) {
-    int days = ((timestamp - SYEAR * getYearOffset(timestamp)) / SDAY) - getVysDays(getYear(timestamp));
+    int days = ((timestamp - SYEAR * getYearOffset(timestamp)) / SDAY) - getLeapDays(getYear(timestamp));
 
     int month = 0;
     while (days - months[month] >= 1) {
@@ -44,8 +45,7 @@ int getMonth(long timestamp) {
 }
 
 int getDay(long timestamp) {
-
-    int days = getDays(timestamp) - getVysDays(getYear(timestamp));
+    int days = getDays(timestamp) - getLeapDays(getYear(timestamp));
 
     for (int i = 0; i < getMonth(timestamp) - 1; i++) {
         if ((i == 1) && ((getYear(timestamp) % 4) == 0)) {
@@ -71,7 +71,7 @@ int getSecond(long timestamp) {
 }
 
 long toUnixTime(int year, int month, int day, int hour, int minute, int second) {
-    int days = --day + getVysDays(year);
+    int days = --day + getLeapDays(year);
     for (int i = 0; i < month - 1; i++) {
         days += months[i];
     }
@@ -82,18 +82,20 @@ long toUnixTime(int year, int month, int day, int hour, int minute, int second) 
 int main() {
     using namespace std;
 
-    cout << getYear(1424855329) << "/" << getMonth(1424855329) << "/" << getDay(1424855329) << " "
-         << getHour(1424855329) << ":" << getMinute(1424855329) << ":" << getSecond(1424855329) << "\n";
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1424855329), getMonth(1424855329), getDay(1424855329), getHour(1424855329), 
+    	getMinute(1424855329), getSecond(1424855329));
 
-    cout << getYear(1000000000) << "/" << getMonth(1000000000) << "/" << getDay(1000000000) << " "
-         << getHour(1000000000) << ":" << getMinute(1000000000) << ":" << getSecond(1000000000) << "\n";
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1000000000), getMonth(1000000000), getDay(1000000000), getHour(1000000000), 
+    	getMinute(1000000000), getSecond(1000000000));
 
-    cout << getYear(1234567890) << "/" << getMonth(1234567890) << "/" << getDay(1234567890) << " "
-         << getHour(1234567890) << ":" << getMinute(1234567890) << ":" << getSecond(1234567890) << "\n";
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1234567890), getMonth(1234567890), getDay(1234567890), getHour(1234567890), 
+    	getMinute(1234567890), getSecond(1234567890));
 
-    cout << toUnixTime(2015, 2, 25, 11, 34, 30) << "\n";
-    cout << getYear(1424864070) << "/" << getMonth(1424864070) << "/" << getDay(1424864070) << " "
-         << getHour(1424864070) << ":" << getMinute(1424864070) << ":" << getSecond(1424864070) << "\n";
+
+    printf("%ld\n", toUnixTime(2015, 2, 25, 11, 34, 30));
+
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1424864070), getMonth(1424864070), getDay(1424864070), getHour(1424864070), 
+    	getMinute(1424864070), getSecond(1424864070));
 
     return 0;
 }
