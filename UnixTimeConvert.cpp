15c1bba0f4d6aeb1841538a779341886773f3498
sergeioff <sergeioff@mail.ru>
2015-02-26 10:54:44 +0200

    Redundant code replaced

--- UnixTimeConvert.cpp
+++ UnixTimeConvert.cpp
@@ -29,7 +29,7 @@ int getLeapDays(int year) {
 }
 
 int getMonth(long timestamp) {
-    int days = ((timestamp - SYEAR * getYearOffset(timestamp)) / SDAY) - getLeapDays(getYear(timestamp));
+    int days = getDays(timestamp) - getLeapDays(getYear(timestamp));
 
     int month = 0;
     while (days - months[month] >= 1) {
@@ -79,20 +79,20 @@ long toUnixTime(int year, int month, int day, int hour, int minute, int second) 
 }
 
 int main() {
-    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1424855329), getMonth(1424855329), getDay(1424855329), getHour(1424855329), 
-    	getMinute(1424855329), getSecond(1424855329));
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1424855329), getMonth(1424855329), getDay(1424855329), getHour(1424855329),
+        getMinute(1424855329), getSecond(1424855329));
 
-    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1000000000), getMonth(1000000000), getDay(1000000000), getHour(1000000000), 
-    	getMinute(1000000000), getSecond(1000000000));
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1000000000), getMonth(1000000000), getDay(1000000000), getHour(1000000000),
+        getMinute(1000000000), getSecond(1000000000));
 
-    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1234567890), getMonth(1234567890), getDay(1234567890), getHour(1234567890), 
-    	getMinute(1234567890), getSecond(1234567890));
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1234567890), getMonth(1234567890), getDay(1234567890), getHour(1234567890),
+        getMinute(1234567890), getSecond(1234567890));
 
 
     printf("%ld\n", toUnixTime(2015, 2, 25, 11, 34, 30));
 
-    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1424864070), getMonth(1424864070), getDay(1424864070), getHour(1424864070), 
-    	getMinute(1424864070), getSecond(1424864070));
+    printf("%04i/%02i/%02i %02i:%02i:%02i\n", getYear(1424864070), getMonth(1424864070), getDay(1424864070), getHour(1424864070),
+        getMinute(1424864070), getSecond(1424864070));
 
     return 0;
 }
